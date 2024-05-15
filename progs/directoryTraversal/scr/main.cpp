d48bb57ded85e995796f89faa841732a4c60f642
TasteOfOil <l.kubrina69@gmail.com>
2024-05-15 12:33:58 +0300

    add recursion

--- progs/directoryTraversal/scr/main.cpp
+++ progs/directoryTraversal/scr/main.cpp
@@ -1,44 +1,53 @@
 #include <iostream>
+#include <cstring>
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
+
 using namespace std;
+
+void readDir(DIR *dir, struct dirent *entry, string path);
 
 int main(int argc, char *argv[]){
 	
 	if (argc==1){
 		return 0;
 	}
-	int size = argc - 1;
-	char **buf = new char*[size];
-	for(int i = 0;i<size;i++){
-		buf[i] = argv[i+1]; 
-	}
-	
-	for(int i = 0;i<size;i++){
-	cout << buf[0];
-	}
+
+	string path;
+	path = argv[1];
+	cout <<path[0];
 	cout<<endl<<"--------------------"<<endl;
 	
 	DIR *dir;
 	struct dirent *entry;
-	dir = opendir(buf[0]);
-	cout <<dir<<endl;
-	if(!dir){
-		perror("diropen");
-		exit(1);
-	}	
-	//вынести в рекурсию
-	while((entry = readdir(dir))!=NULL){
-		if(entry->d_type == DT_DIR){
-		//Входим в подкаталог
-		}
-		
-		cout <<entry->d_name<<" "<<entry->d_type<<endl;
-			
-	}
+	dir = opendir(path.data());
+	//cout <<dir<<endl;
+	readDir(dir,entry, path);
 	closedir(dir);
 	
 	return 0;	
 }
 
+
+void readDir(DIR *dir, struct dirent *entry, string path){
+	if(!dir){
+		perror("diropen");
+		exit(1);
+	}
+		
+	
+	string tempStr;
+	while((entry = readdir(dir))!=NULL){
+		if(entry->d_type == DT_DIR && entry->d_name[0] != '.'){
+			cout <<"Catalog: "<<entry->d_name<<endl;
+			tempStr = path + "/" + entry->d_name;
+			//tempDir = opendir(tempStr.data());
+			readDir(opendir(tempStr.data()),entry,tempStr);	
+		}
+		else if(entry->d_type == DT_REG){
+			cout <<entry->d_name<<" "<<endl;
+		}
+	}
+	//closedir(tempDir);
+}
