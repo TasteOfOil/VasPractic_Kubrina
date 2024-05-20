a34232c270dd068b48219fcdac33d927a08e552d
TasteOfOil <l.kubrina69@gmail.com>
2024-05-20 18:33:06 +0300

    add fork and recursiya

--- progs/directoryTraversal/scr/main.cpp
+++ progs/directoryTraversal/scr/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <ctime>
+#include <signal.h>
 #include <cstring>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -9,74 +12,121 @@
 
 using namespace std;
 
-void readDir(int fd, int count);
+void readDir(int fd, int count, bool flag);
 
 int main(int argc, char *argv[]){
-	
 	if (argc==1){
 		return 0;
 	}
-
+	bool flag = true;
 	string path;
-	path = argv[1];
-	
+	if(argc == 3){
+		flag = stoi(argv[1]);
+		path = argv[2];	
+	}
+	else{
+		path = argv[1];
+	}
 	int fd ;
 	if((fd = open(path.data(),O_RDONLY)) == -1){
 		perror("Error open file");
 		exit(1);
 	}
       	struct stat buf;
-	int res ;
+	int res;
 	if((res = fstat(fd, &buf))==-1){
 		perror("Error");
 		exit(1);
 	}
+	unsigned int start_time =  clock();
 	if(S_ISDIR(buf.st_mode)){
 		try{
-			readDir(fd,0);	
+                  	readDir(fd,0, flag);
+                }
+                catch(const char* error_mes){
+                        cout <<error_mes<<endl;
+                        exit(1);
 		}
-		catch(const char* error_mes){
-			cout <<error_mes<<endl;
-			exit(1);
-		}
+		unsigned int end_time = clock();
+    		unsigned int search_time = end_time - start_time;
+		cout <<"Время работы программы: "<<search_time<<endl;
 	}
+	else {
+		cout <<"This is not a catalog!"<<endl;
+	}
+	
 	close(fd);
 	return 0;	
 }
 
 
 
-void readDir(int fd, int count){
-	++count;
+void readDir(int fd, int count, bool flag){
+	pid_t pid;
+	int status;
+
 	if(count > 20){
-		//cout <<"END"<<endl;
 		throw "Error::looping";
 	}	
-      	DIR * d_fd;
+	++count;
+	struct stat buf;
+	int res;
+	DIR * d_fd;
 	if((d_fd = fdopendir(fd)) == NULL){
 		perror("Error open dir");
 		exit(1);
 	}
 	struct dirent *entry;
+	
+	//cout <<"PID: "<<getpid()<<endl;
 	while ((entry = readdir(d_fd)) != NULL){
 		if(entry->d_name[0]=='.'){
 			continue;
 		}	
 		switch(entry->d_type){
 			case DT_DIR:
-				//cout <<"DIR: "<<count <<" "<<entry->d_name<<endl;
-				cout <<entry->d_name<<endl;
-				readDir(openat(fd,entry->d_name,O_RDONLY), count);
+				if(flag && count>1){
+					pid = fork();
+				}
+				else{
+					pid = -1;
+				}
+				if(pid == 0||pid==-1){//уходим на рекурсию в ребенке
+					readDir(openat(fd,entry->d_name,O_RDONLY), count, flag);
+					if(pid == 0)_exit(status);
+				}
 				break;
 			case DT_LNK:
-				cout <<"link"<<endl;
-				//readDir(openat(fd,entry->d_name, O_RDONLY), count);
+				res = openat(fd,entry->d_name, O_RDONLY);
+				if(fstat(res,&buf) == -1){
+					perror("Error:");
+				}
+				if(S_ISREG(buf.st_mode)){
+					cout <<count<<" "<<entry->d_name<<endl;
+				}
+				else if(S_ISLNK(buf.st_mode)||S_ISDIR(buf.st_mode)){
+					if(flag&& count >1){
+						pid = fork();
+					}
+					else{
+						pid = -1;	
+					}
+					if(pid == 0 || pid == -1){
+						readDir(res,count, flag);
+						if(pid==0)_exit(status);
+					}
+				}
 				break;
 			case DT_REG:
-				//cout <<"file: "<<count<<" "<<entry->d_name<<endl;
 				cout <<entry->d_name<<endl;
 				break;
 		}
+	}
+	if(pid!=0 && flag && pid!=-1){
+		//cout <<"PID "<<pid <<"end"<<endl;
+		waitpid(pid, &status, 0);
+		
+		
 	}	
-	closedir(d_fd);	
+	closedir(d_fd);
 }
