9081f6a3c964e5e6ed9079fe74650cacbec1b9d1
TasteOfOil <l.kubrina69@gmail.com>
2024-06-05 18:15:24 +0300

    add kostilniy cur_market

--- progs/currency_market/scr/main.cpp
+++ progs/currency_market/scr/main.cpp
@@ -12,8 +12,8 @@ using namespace std;
 
 #define MIN_BET 25 
 #define MAX_BET 50
-#define START_BET 50 
-#define END_BET 100
+#define START_BET 60 
+#define END_BET 70
 struct Gamer {
 	int id;
 	int gamers_count;
@@ -34,11 +34,8 @@ int _rand(int start, int end);
 
 void _gamer(Gamer gamer, int *pipefd);
 void cur_market(int N,int **pipefd);
-int findIndex(int **pipefd, int N, int n, struct epoll_event *events);
+int findIndex(int **pipefd, int N, struct epoll_event *events);
 void printBet(double buf[3]);
-//print ставка {от кого, кому, сколько}
-//print результат обмена {ставка}
-//print статус игрока?? бюджет игрока??
 
 
 int main(){
@@ -46,17 +43,22 @@ int main(){
 	cout << "Введите количество игроков: ";
 	cin>>N;
 	pid_t pid = 1;
+	int res = 0;
 	pid_t main_pid = getpid();
 	int **pipefd = new int*[N];
 	for(int i = 0;i<N;i++){
 		pipefd[i] = new int[2];
 	}
-	double start_money = _rand(START_BET,END_BET)/1.0;
+	double start_money = _rand(START_BET,END_BET)/100.0;
 	cout <<"Стартовый капитал: "<<start_money<<endl;
 	for(int i = 0;i<N && pid > 0;i++){
 		if(pid>0){
 			//cout <<"Parent make "<< i+1<<" child"<<endl;
-			if(main_pid == getpid()) {pipe(pipefd[i]);};
+			if((res = pipe(pipefd[i]))==-1 ){
+				perror("pipe");
+				return -1;
+			}
+			fcntl( pipefd[i][0], F_SETFD, O_NONBLOCK);
 			pid = fork();
 			if(pid == 0){
 				Gamer g;
@@ -83,77 +85,94 @@ int main(){
 }
 
 //логика детей
-//передать дескрипторы, записать в структуру????
 void _gamer(Gamer gamer, int *pipefd){
-	//gamer.printGamer();
-	//cout <<endl;
 	
 	double buf_sale[3];
-	//double buf_pur[2];
-	//double buf_res;
+	double buf_res[3];
 	int res;
 	while(gamer.checkBankrupt()){
-		//gamer.printGamer();
 		if(!gamer.status && gamer.checkBankrupt()){//продаем
-			//gamer.printGamer();
 			buf_sale[0] = gamer.id;
 			res = gamer.id;
 			while(res == gamer.id){
-				res = _rand(1, gamer.gamers_count);
-				//cout <<res<<endl;
+				res = _rand(1, gamer.gamers_count) - 1 ;
 			}
 			buf_sale[1] = res;
 			buf_sale[2] = _rand(MIN_BET, MAX_BET)/100.0;
+			//cout <<endl <<"- - - * Раунд " << " * - - -"<<endl;
+			
 			//cout << "Игрок "<<buf_sale[0]<<" продает Игроку "<<buf_sale[1]<<" ставка "<< buf_sale[1]<<endl;
-			res = write(pipefd[1], buf_sale, sizeof(buf_sale));
-			fcntl( pipefd[0], F_SETFD, O_NONBLOCK);
-			if((res = read(pipefd[0], &buf_sale, sizeof(buf_sale))) == -1){
+			res = write(pipefd[1], &buf_sale, sizeof(buf_sale));
+			//fcntl( pipefd[0], F_SETFD, O_NONBLOCK);
+			sleep(2);
+			//while((res = read(pipefd[0], &buf_sale, sizeof(buf_sale))) <= 0){
+			//cout <<"do read "<<buf_sale[0]<<" "<<buf_sale[1]<<" "<<buf_sale[2]<<endl;
+			cout <<endl<<"* * * Продажа * * *"<<endl;
+			printBet(buf_sale);
+			if((res = read(pipefd[0], &buf_res, sizeof(buf_res))) <= 0){
 				cout <<"0 byte read"<<endl;
 			}
+			//}
 			else {
-				printBet(buf_sale);
-				cout <<endl;
-				gamer.money+=buf_sale[2];
+			//cout <<"res "<<res<<endl;
+			//cout <<"after read"<<buf_res[0]<<" "<<buf_res[1]<<" "<<buf_res[2]<<endl;
+				//printBet(buf_sale);
+				//cout <<endl;
+				gamer.money+=buf_res[2];
 				
-				cout<<endl <<"Игрок "<<gamer.id<<": "<<gamer.money<<" валюты"<<endl;
+				cout<<endl <<"Игрок "<<gamer.id<<": "<<gamer.money<<" валюты"<<gamer.status<<endl;
 				if(!gamer.checkBankrupt()){
 					cout <<"Банкрот"<<endl;
 				}
 				else {
 					cout <<"Еще в игре"<<endl;
-					gamer.status = !gamer.status;
+					//gamer.status = !gamer.status;
 				}	
 			}
 			
 		}
-		else{//покупаем
-			fcntl( pipefd[0], F_SETFD, O_NONBLOCK);
-                	while (read(pipefd[0], &buf_sale, sizeof(buf_sale))){
+		else if(gamer.status){//покупаем
+			//fcntl( pipefd[0], F_SETFD, O_NONBLOCK);
+                	sleep(3);
+			while (read(pipefd[0], &buf_sale, sizeof(buf_sale))==-1){
                 	        cout <<"Игрок "<<gamer.id<< " wait..."<<endl;
                 	}
+			//cout <<" GAMER "<<gamer.id<<endl;
 			
-			cout << "Игрок "<<buf_sale[0]<<" продает Игроку "<<gamer.id<<" ставка "<< buf_sale[1]<<endl;
+			cout <<endl <<"- - - * Раунд " << " * - - -"<<endl;
+			//cout << "Игрок "<<buf_sale[0]<<" продает Игроку "<<gamer.id<<" ставка "<< buf_sale[2]<<endl;
+			
 			double bet = _rand(MIN_BET,MAX_BET)/100.0; 
-			double buf_res = bet - buf_sale[1];
+			double buf_res = bet - buf_sale[2];
 			if(buf_res < 0){//забирает себе возвр отриц
 				gamer.money-= buf_sale[2];
 
 			}
 			else if(buf_res > 0){//отнимает себе возвр полож
 				gamer.money -= buf_sale[2];			
-			}	
-			write(pipefd[1], &buf_sale, sizeof(buf_sale));
+			}
+			cout<<endl <<"Игрок "<<gamer.id<<": "<<gamer.money<<" валюты "<<gamer.status<<endl;
+                                if(!gamer.checkBankrupt()){
+                                        cout <<"Банкрот"<<endl;
+                                }
+                                else {
+                                        cout <<"Еще в игре"<<endl;
+                                        //gamer.status = !gamer.status;
+                                }
+			int temp = buf_sale[1];
+			buf_sale[1] = buf_sale[0];
+			buf_sale[0] = temp;	
+			cout <<endl<<"* * * Покупка * * *"<<endl;
+			printBet(buf_sale);
+			res = write(pipefd[1], &buf_sale, sizeof(buf_sale));
+		
 		}
 		sleep(2);
 		gamer.status = !gamer.status;
 	}
-	
-	//if stat true 
-	//продаем и ждем, получаем, проверяем банкротство, меняем статус
-	//if stat false
-	//ждем обрабатываем ставку и отправляем, меняем статус
-
 }
+
+//рандомим ставку
 int _rand(int start, int end) {
         srand(time(0));
 	return rand() % (end - start + 1) + start;
@@ -162,8 +181,7 @@ int _rand(int start, int end) {
 //логика родителя
 void cur_market(int N,int **pipefd){
 	static int round_num = 1;
-	cout <<"- - - * Раунд " <<round_num++ << " * - - -"<<endl;
-	//int status;
+	int res = 0;
 	int index_come;
 	int index_go;
 	double buf_res[3];
@@ -179,58 +197,64 @@ void cur_market(int N,int **pipefd){
 		event[i].data.fd = pipefd[i][0];
 		epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pipefd[i][0], &event[i]);
 	}
-		
+	
+	int n = -2;
+	while(n!=-1){
+	/*struct epoll_event event[N];
+        for(int i = 0;i<N;i++){ 
+                event[i].events = EPOLLIN;
+                event[i].data.fd = pipefd[i][0];
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pipefd[i][0], &event[i]);
+        }
+	*/
 	struct epoll_event events[N];
-		
-	//int n = 0;
-
-			//cout <<"AAAAAAAAAA parent"<<endl;
-		
-		int n = epoll_wait(epoll_fd, events, N, 1000); // -1 означает бесконечное ожидание
-                if (n == -1) {//если сюда, то чета стопнулос
+		int n = epoll_wait(epoll_fd, events, N, 5000); // -1 означает бесконечное ожидание
+		if (n == -1) {//если сюда, то чета стопнулос
 	              	perror("epoll_wait");
 			cout <<"AAAAAAAAAA parent END"<<endl;
-
-                                //close(pipefd[0][0]);
-                                //close(pipefd[0][1]);
                         return;
                 }
                 else{
 			for(int i = 0;i<n;i++){
-			//cout <<"Yes parent"<<endl;
-			index_come = findIndex(pipefd, N, n, events);
-                        //cout <<"***"<<index_come<<"***"<<endl;
-				
+				//cout <<"- - - * Раунд " <<round_num <<" n "<< n<< " * - - -"<<endl;
+                        	//round_num++;
+				index_come = findIndex(pipefd, N, events);
+				//cout<<"index_come "<<index_come<<endl;
 			//sleep(1);
 			//fcntl( pipefd[index_come][0], F_SETFD, O_NONBLOCK);
-			//cout <<"AAAAAAAAAA parent"<<endl;
-			read(pipefd[index_come][0], &buf_res, sizeof(buf_res));
-			//cout <<"AAAAAAAAAA parent"<<endl;
-			printBet(buf_res);
-			index_go = buf_res[1];
+				//cout <<"parent read"<<endl;
+				if ((res = read(pipefd[index_come][0], &buf_res, sizeof(buf_res))) == -1 ){
+					perror("read_market");
+				}
+				//cout <<"parent end read"<<endl;
+				printBet(buf_res);
+				//sleep(1);
+				index_go = buf_res[1];
 			//fcntl( pipefd[int(index_go)][1], F_SETFD, O_NONBLOCK);
-			write(pipefd[(int)index_go][1], &buf_res, sizeof(buf_res));
-			}
+				//cout <<"parent write "<<index_go<<endl;
+				if((res = write(pipefd[(int)index_go][1], &buf_res, sizeof(buf_res))) == -1){
+					perror("write_market");
+				}
+				//cout <<endl<<"parent end write"<<endl;
+				}
                 }
-
 	}
+}
 
 
-
-//логика родителя
-//получит массив дескрипторов, epoll - отслеживание событий
-//
-int findIndex(int **pipefd, int N, int n, struct epoll_event *events){
-	for(int i = 0;i<n;i++){
-		for(int j = 0;j<n;j++){
-			if(pipefd[i][0]==events[j].data.fd){
-				return i;
-			}
+//ищет индекс в массиве pipefd по дескриптору
+int findIndex(int **pipefd, int N, struct epoll_event *events){
+	for(int j = 0;j<N;j++){
+		if(pipefd[j][0]==events[0].data.fd){
+			cout <<"j "<<j<<endl;
+			return j;
 		}
 	}
+	
 	return -1;
 }
 
+//печатает ставку в виде от кого кому и сколько
 void printBet(double buf[3]){
 	cout <<"От Игрока "<<buf[0]<<" Игроку "<<buf[1]<<" Ставка: "<<buf[2]; 
 }
